3fbbda4ba571a1f1bd9411cd6fe52aba61dc2263
andrewdc92 <andrew.cordivari@gmail.com>
2016-08-23 19:22:37 -0700

    ajax works,converts imgs, might have conflict between referenced api links

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,49 @@
+
+var giphy_api = "http://api.giphy.com/v1/gifs/search";
+
+
+
+
+// var giphy_api = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+
 $(document).on("ready", function(){
 
+    // var giphy_api = "http://api.giphy.com/v1/gifs/search";
+
+    appendImages();
+
+  // $('.gif-gallery')
+
+  $("form").on('submit', function(event) {
+
+    event.preventDefault();
+
+    appendImages();
+
+  });
 });
+
+function appendImages() {
+    $.ajax({
+      method: "GET",
+      url: giphy_api,
+      data: $("form").serialize(),
+      success: onSuccess,
+      error: onError,
+    });
+}
+  function onSuccess(json) {
+    // console.log('json works', json); it does work
+    $(".gif-img").remove(); //remove applies to the node, hence removing all imgs
+    json.data.forEach(function(v,i){
+      $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
+  });
+}
+
+  function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+  }
